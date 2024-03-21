d664e036623d77168c4a31734cc2ec2bd79ac69c
LisaFloat <156239252+LisaFloat@users.noreply.github.com>
2024-03-20 23:47:52 -0400

    CarApp 03-20-24
    new implements by Lisa Nguyen

---
+++ META-INF/styling/script.js
@@ -0,0 +1,13 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+    window.location.href = "login.html"; // Redirect to login page
+});
+
+document.getElementById("signupBtn").addEventListener("click", function() {
+    window.location.href = "signup.html"; // Redirect to sign up page
+});
+
+document.getElementById("loginBackBtn").addEventListener("click", function() {
+    window.location.href = "index.html"; // Redirect to index.html
+});
+
+
